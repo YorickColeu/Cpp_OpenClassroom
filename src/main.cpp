cc23bcb2215b9f38d0942b6dbfb5a14c83db7a8f
yorickcoleu <yorick.coleu@gmail.com>
2016-11-20 20:37:42 +0100

    Calculator done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world" << endl;
+    cout << "Entrer nb1" << endl;
+    double nb1(0.0);
+    cin >> nb1;
+
+    cout << "Entrer nb2" << endl;
+    double nb2(0.0);
+    cin >> nb2;
+
+    cout << "Result:" << nb1+nb2 << endl;
+
+    return 0;
 }
+
